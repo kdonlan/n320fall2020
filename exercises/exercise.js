bace31214f2ec76ef327b6e8276e044624b17148
Kim Donlan <32644497+kdonlan@users.noreply.github.com>
2020-08-27 21:28:43 -0400

    fixed console again

--- exercises/exercise.js
+++ exercises/exercise.js
@@ -11,40 +11,41 @@
 //     myHouse.show();
 
 // defining a class
-    // class House {
-    //     constructor(street, number, zipcode) {
-    //         this.street = street;
-    //         this.number = number;
-    //         this.zipcode = zipcode;
-    //     }
-    //     show() {
-    //         console.log("My house address is " + this.number + " " + this.street + " in Indianapolis, IN.");
-    //     }
-    // }
+    class House {
+        constructor(street, number, zipcode) {
+            this.street = street;
+            this.number = number;
+            this.zipcode = zipcode;
+        }
+        show() {
+            console.log("My house address is " + this.number + " " + this.street + " in Indianapolis, IN.");
+        }
+    }
 
-    //creating a new instance of a class / the "new" keyword runs the constructor above
-    // var myHouse = new House("Elm",123,44578);
+    // creating a new instance of a class / the "new" keyword runs the constructor above
+    var myHouse = new House("Elm",123,44578);
     
-    //invoke the class method
-    // myHouse.show();
+    // invoke the class method
+    myHouse.show();
 
 //inheritance - define
-class Building {
-    constructor(street, number, zipcode) {
-        this.street = street;
-        this.number = number;
-        this.zipcode = zipcode;
-    }
-}
+// class Building {
+//     constructor(street, number, zipcode) {
+//         this.street = street;
+//         this.number = number;
+//         this.zipcode = zipcode;
+//     }
+// }
 
 //extending a class
-class House extends Building {
-    constructor(street,number,zipcode) {
-        super(street,number,zipcode);
-        this.kind = "House;"
-    }
-    show() {
-        console.log(this.street + "St is such a nice place to live.");
-    }
-}
+// class House extends Building {
+//     constructor(street,number,zipcode) {
+//         super(street,number,zipcode);
+//         this.kind = "House;"
+//     }
+//     show() {
+//         console.log(this.street + "St is such a nice place to live.");
+//     }
+// }
 
+// console.log(House);
