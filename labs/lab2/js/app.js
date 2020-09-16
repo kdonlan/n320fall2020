d216058af30b76689420a2d63f16bf5cca0f52b7
Kim Donlan <32644497+kdonlan@users.noreply.github.com>
2020-09-15 20:53:54 -0400

    Inheriting Instruments complete

--- labs/lab2/js/app.js
+++ labs/lab2/js/app.js
@@ -1,31 +1,36 @@
 //base abstract class
 
 class Instrument {
-    constructor(loudness,family) {
-    this.loudness = loudness;
-    this.family = family;
+    constructor(loudness) {
+        this.loudness = loudness;
 }
     playInstrument() { //the verb to play the instrument that will print info to the console
-        console.log("The " + instruments + " plays at " + this.loudness + " decibels and belongs to the " + this.family + " family.");
+        console.log("The " + this.name + " plays at " + this.loudness + " decibels and belongs to the " + this.family + " family.");
     }
 }
 
 //child class
 class Clarinet extends Instrument {
-    constructor() {
-        super(90, "Woodwind");
+    constructor(name, family) {
+        super("90");
+        this.name = "Clarinet";
+        this.family = "Woodwind";
         }
 }
 //child class
 class Guitar extends Instrument {
-    constructor() {
-        super(81, "String");
+    constructor(name, family) {
+        super("81");
+        this.name = "Guitar";
+        this.family = "Strings";
         }
 }
 //child class
 class Drum extends Instrument {
-    constructor() {
-        super(120, "Percussion");
+    constructor(name, family) {
+        super("120");
+        this.name = "Drum";
+        this.family = "Percussion";
         }
 }
 
