725b371e0000b0596b85084d232343b128ae32e5
Kim Donlan <32644497+kdonlan@users.noreply.github.com>
2020-09-22 19:04:54 -0400

    with pictures

--- labs/lab3/js/app.js
+++ labs/lab3/js/app.js
@@ -1,29 +1,43 @@
-class firstCandy {
-    constructor(name, inventory) {
+class Item {
+    constructor(name, inventory, image) {
         this.name = name;
         this.inventory = inventory;
+        this.image = image;
     }
-    render() {
-        return `
-            <div>${this.name}: ${this.inventory}</div>
-                  <div>Current Stock: ${this.newCount} </div>
-                `;
+    render(newCount) {
+        return `<img style="width:300px;"src="${this.image}"><div><h3>${this.name}</h3></div>
+            <button onclick="buyItem()">Buy ${this.name}</button>
+            Inventory: ${this.inventory}</div><br>`;
     }
     
-    buyCandy(newCount) {
+    buyItem(newCount) {
         if (this.inventory > 0) {
-           this.inventory --;
-            console.log(this.inventory);
-            console.log(newCount);
-            console.log("Buying the First Candy");
+            newCount = this.inventory--;
+            // console.log(this.inventory);
+            // console.log(newCount);
+            // console.log("Buying the First Item");
         }
     }
 }
 
-let vending1 = new firstCandy("Snickers", 30);
+let vending1 = new Item("Snickers", 30, "./images/snickers.jpg");
 let vendDiv = document.getElementById("vendDiv");
 vendDiv.innerHTML = vending1.render();
-function buyCandy() {
-    vending1.buyCandy();
+
+let vending2 = new Item("Twix", 80,"./images/twix.jpg");
+let vendDiv2 = document.getElementById("vendDiv2");
+vendDiv2.innerHTML = vending2.render();
+
+let vending3 = new Item("Doritos", 50,"./images/doritos.jpg");
+let vendDiv3 = document.getElementById("vendDiv3");
+vendDiv3.innerHTML = vending3.render();
+
+function buyItem() {
+    vending1.buyItem();
+    vending2.buyItem();
+    vending3.buyItem();
     vendDiv.innerHTML = vending1.render();
+    vendDiv2.innerHTML = vending2.render();
+    vendDiv3.innerHTML = vending3.render();
+
 }
