4414d6398d799e264a58d6d7e8e56011205b3c3b
Kim Donlan <32644497+kdonlan@users.noreply.github.com>
2020-12-09 15:17:42 -0500

    added more movement - data in but loop not working

--- final/app/app.js
+++ final/app/app.js
@@ -1,5 +1,23 @@
 console.log("file is working");
 
+jQuery(document).ready(function(){
+	jQuery(".titleWrapper").addClass("ready");
+	
+jQuery(".titleWrapper h1").each(function(){
+	var fullString;
+	var characters = jQuery(this).text().split("");
+	
+	$this = jQuery(this);
+	$this.empty();
+	$.each(characters, function (i, el) {
+		if(el == " "){el = "&nbsp;"};
+    $this.append("<span>" + el + "</span");
+	});
+		
+});
+	
+});
+
 ///////////////////////////////////////////////////////////////////////// LOADING DATA SET ///////////////////////////////////////////////////
 //json file brought in using fetch in order to access the json file
 fetch("data/data.json")
@@ -10,16 +28,21 @@ fetch("data/data.json")
         $(document).ready(function () {
             
             $.each(data, function(idx, value) {
+                
                 console.log(value);
                 console.log(value.firstName)
-                // for (i = 0; i <= players.length; i++) {
+                console.log(value.city)
+                console.log(value.game)
+
+                    
                 player = $(".custom-row .tab-text").append(
                     `<h2>${value.firstName}</h2>
                     <h3 style="font-size: 1rem;margin-top:10px; text-align: left;">${value.city}, IN</h3>
                     <p>${value.game}</p>
                     <div class="icons-block"> <a href="#" class="social-icon-1"><i class="fa fa-facebook-official"></i></a> <a href="#" class="social-icon-2"><i class="fa fa-twitter-square"></i></a> <a href="#" class="social-icon-3"><i class="fa fa-youtube-square"></i></a> </div>`);
-                // }
+                
             })
+            console.log(data.length);
         });
     })
 
--- final/app/app2.js
+++ final/app/app2.js
@@ -1,35 +1,55 @@
-let players = [];
 
 //describes the player class
 class Player {
-    constructor(firstName, city, game) {
+    constructor(firstName, city, game) { //need to add story in constructor for flip eventually
         this.firstName = firstName;
         this.city = city;
         this.game = game;
-        // this.story = story; //need to add story in constructor for flip
+        // this.story = story; 
     }
+    //puts the player's record on the page within the div
     renderPlayerInfo() {
-            return player = new Player().append(
-                `<h2>${this.firstName}</h2>
-                <h3 style="font-size: 1rem;margin-top:10px; text-align: left;">${this.city}, IN</h3>
-                <p>${this.game}</p>`
+        if(i = 0, i <= data.length, i++)
+            players.forEach((player) => {
+            return player = $(".custom-row .tab-text").append(
+                `<div><h2>${this.firstName}</h2>
+                    <h3 style="font-size: 1rem;margin-top:10px; text-align: left;">${this.city}, IN</h3>
+                    <p>${this.game}</p></div>`
             )
-        }
-    }
 
-fetch("data/data.json")
+            })
+        
+}
+}
+
+    fetch("data/data.json")
     .then(response => {
         return response.json();
     })
     .then((data) => {
+        console.log(data);
+        $(document).ready(function () {
+            // let player = new Player(this.firstName.value, this.city.value, this.game.value);
+            let data = new Player(firstName.value, data.city.value, data.game.value);
 
-    for ( i= 0; i<= 4; i++) {
-        let players = new Player([data[i]]);
-        console.log(data[i]);
-        let player = new Player(data.firstName, data.city, data.game);
-        console.log(this.player);
-}
+            this.player.renderPlayerInfo();  
+            })
+        })
 
-// players.forEach(player.renderPlayerInfo());
-//         console.log(data[i]);
-    })
+
+
+
+
+
+
+    // for (i = 0; i <= players.length; i++) {
+    //     console.log(this.value);
+    //     console.log(this.value.firstName);
+    //     let player = new Player();
+    //     players.forEach((player) => {})
+    //     $(".custom-row .tab-text").append(
+    //         `<h2>${value.firstName}</h2>
+    //         <h3 style="font-size: 1rem;margin-top:10px; text-align: left;">${this.value.city}, IN</h3>
+    //         <p>${value.game}</p>
+    //         <div class="icons-block"> <a href="#" class="social-icon-1"><i class="fa fa-facebook-official"></i></a> <a href="#" class="social-icon-2"><i class="fa fa-twitter-square"></i></a> <a href="#" class="social-icon-3"><i class="fa fa-youtube-square"></i></a> </div>`);
+    //     }
