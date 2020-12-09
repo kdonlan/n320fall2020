afeb685d26dfa29582f9557b7c35f7bd63373150
Kim Donlan <32644497+kdonlan@users.noreply.github.com>
2020-12-09 09:30:07 -0500

    progress on final

--- final/app/app2.js
+++ final/app/app2.js
@@ -2,53 +2,34 @@ let players = [];
 
 //describes the player class
 class Player {
-    constructor(firstName, city, game, story) {
+    constructor(firstName, city, game) {
         this.firstName = firstName;
         this.city = city;
         this.game = game;
-        this.story = story;
+        // this.story = story; //need to add story in constructor for flip
     }
-    render() {
-        return `<h2>${this.firstName}</h2>
-        <h3 style="font-size: 1rem;margin-top:10px; text-align: left;">${this.city}, IN</h3>
-        <p>${this.game}</p>`
+    renderPlayerInfo() {
+            return player = new Player().append(
+                `<h2>${this.firstName}</h2>
+                <h3 style="font-size: 1rem;margin-top:10px; text-align: left;">${this.city}, IN</h3>
+                <p>${this.game}</p>`
+            )
+        }
     }
+
+fetch("data/data.json")
+    .then(response => {
+        return response.json();
+    })
+    .then((data) => {
+
+    for ( i= 0; i<= 4; i++) {
+        let players = new Player([data[i]]);
+        console.log(data[i]);
+        let player = new Player(data.firstName, data.city, data.game);
+        console.log(this.player);
 }
 
-//json file brought in using fetch in order to access the json file
-fetch("data/data.json")
-    .then(function (resp) {
-        return resp.json();
+// players.forEach(player.renderPlayerInfo());
+//         console.log(data[i]);
     })
-    .then(function (data) {
-        $(document).ready(function () {
-
-            $.each(data, function (idx, value) {
-                
-                for (i = 0; i > players.length; i++) {
-                    let player = new Player(this.firstName, this.city, this.game);
-                    console.log(player);
-                    console.log(player.length);
-                    //puts text in the div
-                    let playerCard = document.getElementById(".tab-text");
-                    //puts each player on the page 
-                    player.innerHTML = player.render()
-                }
-
-
-            console.log(value);
-            console.log(value.firstName)
-        })
-    });
-})
-
-
-// //defines the each player
-// for (i = 0; i > players.length; i++) {
-//     let player = new Player(this.firstName, this.city, this.game);
-//     console.log(player);
-//     //puts text in the div
-//     let playerCard = document.getElementById(".custom-row .tab-text");
-//     //puts player on the page 
-//     playerCard.innerHTML = player.render()
-// }
