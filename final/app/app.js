6945cab11d50dc7362c92ae93b0cd6c5262b6bdf
Kim Donlan <32644497+kdonlan@users.noreply.github.com>
2020-12-10 23:04:52 -0500

    started on quotes - styling mostly polished up

--- final/app/app.js
+++ final/app/app.js
@@ -25,14 +25,41 @@ fetch("data/data.json") //path to the data.json file
                 players[idx].innerHTML = 
                     `<h2>${value.firstName}</h2>
                     <h3 style="font-size: 1rem;margin-top:10px; text-align: left;">${value.city}, IN</h3>
-                    <p>${value.game}</p>
-                    <div class="icons-block"> <a href="#" class="social-icon-1"><i class="fa fa-facebook-official"></i></a> <a href="#" class="social-icon-2"><i class="fa fa-twitter-square"></i></a> <a href="#" class="social-icon-3"><i class="fa fa-youtube-square"></i></a> </div>`;
+                    <p>${value.game}</p>`;
                 
             })
             //shows there are 15 objects in the array
             // console.log(data.length); 
         });
+            
+        // $(document).ready(function () { 
+        //     //page loads and players from data.json appends to the custom-row tab-text classes in the mark up HTML
+        //     quotes = $(".custom-row .tab-textb")
+
+        //     $.each(data, function(idx, value) { //loops through each object in the json file
+        //         //console out what the values are
+        //         // console.log(idx); //index 0 - 14 of the array
+        //         // console.log(value); //the object
+        //         // console.log(value.firstName) //value "firstName" of the object
+        //         // console.log(value.city) //value "city" of the object
+        //         // console.log(value.game) //value "game" of the object
+                
+        //         //loops through indexes 0-14 (15 objects in the data)
+        //         //while temperal literal that will populate the div classes we named above; includes the values of keys consoled above
+        //         quotes[idx].innerHTML = 
+        //             `<h2>"</h2>
+        //             <h3 style="font-size: 1rem;margin-top:10px; text-align: left;">${value.story}</h3>
+        //             <h4>"</h4>`;
+        //     })
+        //     //shows there are 15 objects in the array
+        //     // console.log(data.length); 
+        // });
+
+        
     })
+
+
+    
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -54,7 +81,15 @@ jQuery(".titleWrapper h1").each(function(){
 });
 
 //onclick function of the players cards that will show quote
+// function flip() {
+//     console.log("card flips");
+//     $(".effect-twelve").toggleClass(".effect-twelveb");
+//     // $("#front").hide();
+
+// }
+
+
 function flip() {
-    console.log("card flips");
-    // $("oneClass").toggleClass(".anotherClass);
+    $('.card').toggleClass('flipped');
 }
+
